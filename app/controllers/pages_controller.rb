59e2e3d66660f63e752869b864259b9b6d793f1b
leberSmeaton <77179232+leberSmeaton@users.noreply.github.com>
2021-08-02 23:19:56 +1000

    generated new username column to users, created username form field for signup

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,9 @@
 class PagesController < ApplicationController
-    
+    # check that a user is logged in before taking you to the home page
+    before_action :authenticate_user!
+
     def home
     end
+
+    
 end
