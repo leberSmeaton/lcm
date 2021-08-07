63babdd6389e7ea7f3e50cddbab3d59ab8af6ca9
leberSmeaton <77179232+leberSmeaton@users.noreply.github.com>
2021-08-08 01:29:47 +1000

    updated placeholder image and created products helper. Attempting to create another helper method but to no success see trello card.

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,2 +1,7 @@
 module ProductsHelper
+    def image_select(product)
+        return product.product_image if product.product_image.attached?
+        # if no product image attached the default placeholder image will be used.
+        return "placeholder.jpg"
+    end
 end
